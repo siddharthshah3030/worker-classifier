b348695dafb6f91aad3aa6914303ca303ddbe3a3
siddharth shah <siddharthshah3030@gmail.com>
2019-01-25 00:57:30 +0530

    typo for results_count

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ prediction.setModelPath("idenprof_061-0.7933.h5")
 prediction.setJsonPath("idenprof_model_class.json")
 prediction.loadModel(num_objects=10)
 
-predictions, probabilities = prediction.predictImage("image.jpg", result_count=3)
+predictions, probabilities = prediction.predictImage("image.jpg", result_count=)
 
 for eachPrediction, eachProbability in zip(predictions, probabilities):
     print(eachPrediction , " : " , eachProbability)
